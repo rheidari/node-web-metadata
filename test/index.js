52144a069d1413ac213a85b8a477d005fae3c6cc
Ed Moore <eddie.moore@gmail.com>
2016-05-20 10:38:37 +0800

    Updating tests with new links

--- test/index.js
+++ test/index.js
@@ -19,7 +19,7 @@ describe('node-web-metadata', function () {
   });
 
   it('returns data on success', function (done) {
-    metadata({url:'http://leap.it'}, function (err, data) {
+    metadata({url:'http://google.com'}, function (err, data) {
       assert.equal(err, null);
       assert.notEqual(data, null);
       done();
@@ -41,8 +41,9 @@ describe('node-web-metadata', function () {
     fs.readFile('test/html/malformed_multiple_head_tags.html', 'utf8', function (err, data) {
       metadata({html:data}, function (err, data) {
         assert.equal(err, null);
-        assert.equal(data.meta.description, 'test description #2');
-        assert.equal(data.title, 'testing head #2');
+        assert.equal(data.meta.description, 'test description #1');
+        assert.equal(data.title, 'testing head #1');
+        assert.deepEqual(data.meta.descriptions, ['test description #1', 'test description #2'])
         done();
       });
     });
@@ -60,7 +61,7 @@ describe('node-web-metadata', function () {
   });
 
   it('will return contentType for a website url', function (done) {
-    metadata({url:'http://leap.it'}, function (err, data) {
+    metadata({url:'http://google.com'}, function (err, data) {
       assert(data.contentType.indexOf('text/html') !== -1);
       assert.notEqual(data.url, null);
       done();
@@ -68,7 +69,7 @@ describe('node-web-metadata', function () {
   });
 
   it('will return contentType for an image url', function (done) {
-    metadata({url:'http://leap2-marketing.s3.amazonaws.com/leapit-300x300.jpg'}, function (err, data) {
+    metadata({url:'https://placekitten.com/g/200/300'}, function (err, data) {
       assert(data.contentType.indexOf('image/jpeg') !== -1);
       assert.notEqual(data.url, null);
       done();
@@ -76,7 +77,7 @@ describe('node-web-metadata', function () {
   });
 
   it('will return contentType for a video url', function (done) {
-    metadata({url:'http://videos-f-8.ak.instagram.com/hphotos-ak-ash/10381557_1558605304366028_1049389520_n.mp4'}, function (err, data) {
+    metadata({url:'http://scontent-sin1-1.cdninstagram.com/t50.2886-16/13248162_1066593200053350_416643217_n.mp4'}, function (err, data) {
       assert(data.contentType.indexOf('video/mp4') !== -1);
       assert.notEqual(data.url, null);
       done();
